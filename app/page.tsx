103df3e92829443e5146df300cc56f3300e3b019
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2024-04-10 22:11:35 +0800

    fetch data and display on command

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+'use client'
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command"
+
+interface Fixture {
+  title: string;
+  embed: string;
 }
 
-export default page
+const Client = () => { 
+  const [fixtures, setFixtures] = useState<Fixture[]>()
+
+  const getVideos = async () => {
+    const options = {
+      method: 'GET',  
+      url: 'https://free-football-soccer-videos.p.rapidapi.com/',
+      headers: {
+        'X-RapidAPI-Key': process.env.NEXT_PUBLIC_X_RapidAPI_KEY as string,
+        'X-RapidAPI-Host': process.env.NEXT_PUBLIC_X_RapidAPI_HOST as string
+      }
+    };
+    
+    try {  
+      const response = await axios.request(options);
+      setFixtures(response.data as [])
+    } catch (error) {
+      console.error(error);
+    }
+  }
+   
+  useEffect(() => {
+    getVideos()
+  }, [])
+
+  return (
+    <div className='flex flex-col min-h-screen items-center justify-center'>
+        <h1>
+            Recent fixtures
+        </h1>
+        {fixtures ? (
+           <div className='flex flex-row items-center justify-center mt-10'>
+              <Command className='border rounded-md w-[300px]'>    
+                  <CommandInput placeholder="Search for a fixture..." />
+                  <CommandList>
+                    <CommandEmpty>No results found.</CommandEmpty>
+                    <CommandGroup>
+                        {fixtures.map((fixture) => (
+                          <CommandItem className='hover:cursor-pointer'>
+                            {fixture.title}
+                          </CommandItem>
+                        ))}
+
+                    </CommandGroup>
+                  </CommandList>
+
+                  
+              </Command>  
+        </div>
+        ) : (
+          <></>
+        )}
+    </div>  
+  )
+}  
+
+export default Client
